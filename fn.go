91cee969fb659c6ba0bfe84b50954d10002d96bb
Paul Curtis <github@csspc.com>
2017-10-13 08:15:54 -0400

    rename aux files without fn, copyright in each file; ver 0.021

--- fn.go
+++ fn.go
@@ -1,3 +1,7 @@
+// Copyright 2017 phcurtis fn Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package fn - includes APIs relating to function names (fn).
 // Such as returning a given func name relative to its position on the
 // call stack. Other APIs include returning all the func names on the
@@ -17,7 +21,7 @@ import (
 )
 
 // Version of package fn
-const Version = 0.02
+const Version = 0.021
 
 // Level genealogy values for exported Lvl functions
 const (
